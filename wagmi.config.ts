e5209644d42c9e727628f5a2102810e0f00e1aa7
lucyTrump <137249723+lucyTrump@users.noreply.github.com>
2024-05-19 20:13:07 +0800

    Update wagmi.config.ts

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@wagmi/cli';
 import { contracts } from './src/config/contract';
 import { react } from '@wagmi/cli/plugins';
-import {Erc20}
+
 
 export default defineConfig({
 	out: 'src/config/generated.ts',
